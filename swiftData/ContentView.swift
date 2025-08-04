22c30d42f617d01b9e144cbc75b81a1f7bdf30a7
LeFab70 <kouonang2002@gmail.com>
2025-08-04 19:20:34 -0300

    Initial Commit

---
+++ swiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftData
+//
+//  Created by Fabrice Kouonang on 2025-08-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftData/swiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftDataApp.swift
+//  swiftData
+//
+//  Created by Fabrice Kouonang on 2025-08-04.
+//
+
+import SwiftUI
+
+@main
+struct swiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
